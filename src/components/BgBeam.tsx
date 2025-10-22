e52c80476d84f51f4f9be21789439de22ef19f18
Json604 <kartikey060105@gmail.com>
2025-10-23 04:09:22 +0530

    slight animation

--- src/components/BgBeam.tsx
+++ src/components/BgBeam.tsx
@@ -1,11 +1,31 @@
 import './components.css'
-
+import {motion} from 'motion/react'
 
 function BgBeam() {
     return(
         <>
-        <div className="beam left-beam"></div>
-        <div className="beam right-beam"></div>
+        <motion.div
+        className="beam left-beam"
+        initial={{ x: -1000, y: -1000, opacity: 0, rotate: -45 }}
+        animate={{ x: 0, y: 0, opacity: 0.8, rotate: -25 }}
+        transition={{
+            type: "spring",
+            stiffness: 60,
+            damping: 18,
+            duration: 3
+        }}
+        />
+        <motion.div
+        className="beam right-beam"
+        initial={{ x: 1000, y: -1000, opacity: 0, rotate: 45 }}
+        animate={{ x: 0, y: 0, opacity: 0.8, rotate: 25 }}
+        transition={{
+            type: "spring",
+            stiffness: 60,
+            damping: 18,
+            duration: 3
+        }}
+        />
         </>
     )
 }
