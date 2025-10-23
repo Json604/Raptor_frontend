c39341c22047f5cef2dd4331ff735e022e0c096c
Json604 <kartikey060105@gmail.com>
2025-10-23 18:12:52 +0530

    restructuring

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './pages/main/App.tsx'
 import Navbar from './components/Navbar.tsx'
 import BgBeam from './components/BgBeam.tsx'
 
